773d157099ee97569a5ba5a85b3849ae4897c63c
Michal Dadaj (Majkysan) <michaldadaj73@gmail.com>
2024-12-25 21:55:40 +0100

    uprsvenea fyzika a skok

--- 2.5D_Platformer/Assets/Scripts/PlayerController.cs
+++ 2.5D_Platformer/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speedMove = 5f;
     [SerializeField] private float _jumpForce = 5f;
-
+    [SerializeField] private float _maxFallSpeed = -30f;
     // Gravity Scale
     [SerializeField] private float _gravityScale = -9.81f; // Default gravity value
+    [SerializeField] LayerMask _groundLayerMask;
     private float _yVelocity;
     private InputSystem_Actions _actions;
     private CharacterController _controller;
     private Vector2 _moveInput;
     private Vector2 _playerVelocity;
+
+    private bool _isGrounded;
+    private bool _isJump;
+    
     
 
-    private bool _isJump;
-    private void Awake() 
+    private void Awake()
     {
         // Initialize input actions
         if (_actions == null)
@@ -25,23 +29,20 @@ public class PlayerController : MonoBehaviour
             _actions = new InputSystem_Actions();
             _actions.Enable();
         }
-        
+
         // Get the character controller component
         _controller = GetComponent<CharacterController>();
-               
     }
 
-    private void OnEnable() 
+    private void OnEnable()
     {
         // Bind input actions to callbacks
         _actions.Player.Enable();
         _actions.Player.Move.performed += OnMove;
         _actions.Player.Move.canceled += OnMove;
         _actions.Player.Jump.performed += OnJump;
-        _actions.Player.Jump.canceled += OnJump;        
+        _actions.Player.Jump.canceled += OnJump;
     }
-
-    
 
     private void OnDisable()
     {
@@ -50,12 +51,12 @@ public class PlayerController : MonoBehaviour
         _actions.Player.Move.canceled -= OnMove;
         _actions.Player.Jump.performed -= OnJump;
         _actions.Player.Jump.canceled -= OnJump;
-    }    
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -63,9 +64,8 @@ public class PlayerController : MonoBehaviour
     {
         PlayerMove();
         PhysicGravity();
+        
     }
-
-    
 
     private void OnMove(InputAction.CallbackContext context)
     {
@@ -82,31 +82,55 @@ public class PlayerController : MonoBehaviour
 
     private void PhysicGravity()
     {
-        Vector3 gravityScale = new Vector3(0, _gravityScale,0);
+        // Nastavení raycastu
+        Vector3 rayOrigin = transform.position; 
+        Vector3 rayDirection = -Vector3.up; 
+        float rayDistance = 0.1f; // Adjust this distance as needed
 
-        if(_controller.includeLayers != LayerMask.GetMask("Ground"))
+        // Provedení raycastu
+        if (Physics.Raycast(rayOrigin, rayDirection, out RaycastHit hit, rayDistance, _groundLayerMask))
         {
-            _playerVelocity.y += gravityScale.y * Time.deltaTime;
-           Debug.Log("PhysicsForce: " + _playerVelocity.y);
-            _controller.Move(_playerVelocity * Time.deltaTime);
-
+            _isGrounded = true;
         }
         else
         {
-            _playerVelocity.y = 0f;
-            _controller.Move(_playerVelocity * Time.deltaTime);
+            _isGrounded = false;
         }
+
+        if (!_isGrounded)
+        {
+            _yVelocity += _gravityScale * Time.deltaTime;
+
+            // Omezit maximální rychlost pádu pouze při volném pádu
+            if (_yVelocity < _maxFallSpeed && !_isGrounded) 
+            {
+                _yVelocity = _maxFallSpeed; 
+            }
+        }
+        else
+        {
+            _yVelocity = 0f; 
+        }
+
+        Debug.Log("yVelocity: " + _yVelocity);
+        // Apply gravity
+        Vector3 gravity = new Vector3(0, _yVelocity, 0);
+        _controller.Move(gravity * Time.deltaTime);
     }
 
     private void OnJump(InputAction.CallbackContext context)
     {
         _isJump = context.ReadValueAsButton();
 
-        if(_isJump && _controller.isGrounded)
+        if (_isJump && _controller.isGrounded)
         {
-            _playerVelocity.y += Mathf.Sqrt(_jumpForce * -2f * _gravityScale);
-             Debug.Log("jumForces: " + _playerVelocity.y);
+            Debug.LogWarning("jsem na zemi ");
+            _yVelocity = Mathf.Sqrt(_jumpForce * -2f * _gravityScale);
             
-        }       
+        }
+        else
+        {
+            Debug.LogWarning("Nejsem na zemi");
+        }
     }
 }
